a1bca36859a0f837fe76aa19d076630315e8c0da
pppq <pppq@users.noreply.github.com>
2022-02-02 01:09:34 +0100

    Remove character set save/restore pairs from schema.sql
    It's easier on the eyes to set it once at the top of the script, then
    restore the value to the original at the end.

--- schema.sql
+++ schema.sql
@@ -1,36 +1,39 @@
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES */;
+
 /*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40014 SET UNIQUE_CHECKS=0 */;
+/*!40014 SET FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET SQL_NOTES=0 */;
+
+/*!40101 SET CHARACTER_SET_CLIENT='utf8' */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Table structure for table `accesslist`
 --
 
 DROP TABLE IF EXISTS `accesslist`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `accesslist` (
   `entryID` int(10) unsigned NOT NULL DEFAULT '0',
   `userID` int(10) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`entryID`),
   KEY `Index_2` (`userID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `accesslog`
 --
 
 DROP TABLE IF EXISTS `accesslog`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `accesslog` (
   `datum` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `micros` int(3) NOT NULL DEFAULT '0',
@@ -45,30 +48,24 @@ CREATE TABLE `accesslog` (
   KEY `ip` (`ip`),
   KEY `userCode` (`userCode`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `bannedip`
 --
 
 DROP TABLE IF EXISTS `bannedip`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `bannedip` (
   `IP` char(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   `banCode` int(10) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`IP`),
   KEY `bannedIP_banCode` (`banCode`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `bans`
 --
 
 DROP TABLE IF EXISTS `bans`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `bans` (
   `ID` int(10) unsigned NOT NULL DEFAULT '0',
   `userID` int(10) unsigned NOT NULL DEFAULT '0',
@@ -79,30 +76,24 @@ CREATE TABLE `bans` (
   PRIMARY KEY (`ID`),
   KEY `Bans_userID` (`userID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `bots`
 --
 
 DROP TABLE IF EXISTS `bots`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `bots` (
   `ID` int(6) unsigned NOT NULL DEFAULT '0',
   `login` char(30) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   `createDate` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `loginDate` datetime NOT NULL DEFAULT '0000-00-00 00:00:00'
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `cache_commentlist`
 --
 
 DROP TABLE IF EXISTS `cache_commentlist`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `cache_commentlist` (
   `category` enum('comments','commentsOfEntries','myComments') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'comments',
   `ID` int(11) NOT NULL DEFAULT '0',
@@ -118,15 +109,12 @@ CREATE TABLE `cache_commentlist` (
   KEY `ownerID` (`ownerID`),
   KEY `access` (`access`)
 ) ENGINE=MEMORY DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `cache_entrylist`
 --
 
 DROP TABLE IF EXISTS `cache_entrylist`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `cache_entrylist` (
   `ID` int(11) NOT NULL DEFAULT '0',
   `ownerID` int(10) unsigned NOT NULL DEFAULT '0',
@@ -140,15 +128,12 @@ CREATE TABLE `cache_entrylist` (
   KEY `ownerID` (`ownerID`),
   KEY `access` (`access`)
 ) ENGINE=MEMORY DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `calendar`
 --
 
 DROP TABLE IF EXISTS `calendar`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `calendar` (
   `userID` int(10) unsigned NOT NULL DEFAULT '0',
   `y` int(10) unsigned NOT NULL DEFAULT '0',
@@ -164,15 +149,12 @@ CREATE TABLE `calendar` (
   KEY `Calendar_Year` (`y`,`m`),
   KEY `Calendar_Day` (`d`,`m`,`y`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `categories`
 --
 
 DROP TABLE IF EXISTS `categories`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `categories` (
   `entryID` int(11) NOT NULL DEFAULT '0',
   `Name` char(30) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
@@ -180,15 +162,12 @@ CREATE TABLE `categories` (
   KEY `Name` (`Name`),
   KEY `entryID` (`entryID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `commentrates`
 --
 
 DROP TABLE IF EXISTS `commentrates`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `commentrates` (
   `userID` int(11) NOT NULL,
   `commentID` int(11) NOT NULL,
@@ -197,15 +176,12 @@ CREATE TABLE `commentrates` (
   PRIMARY KEY (`userID`,`commentID`),
   KEY `userID` (`userID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `comments`
 --
 
 DROP TABLE IF EXISTS `comments`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `comments` (
   `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `isTerminated` enum('Y','N') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'N',
@@ -225,15 +201,12 @@ CREATE TABLE `comments` (
   KEY `createDate` (`createDate`),
   KEY `isTerminated_2` (`userID`,`isTerminated`,`createDate`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1912242 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `entries`
 --
 
 DROP TABLE IF EXISTS `entries`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `entries` (
   `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `diaryID` int(10) unsigned NOT NULL DEFAULT '0',
@@ -266,30 +239,24 @@ CREATE TABLE `entries` (
   KEY `diaryID_2` (`diaryID`,`userID`,`isTerminated`),
   KEY `posX` (`posX`,`posY`)
 ) ENGINE=MyISAM AUTO_INCREMENT=443165 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `entryaccess`
 --
 
 DROP TABLE IF EXISTS `entryaccess`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `entryaccess` (
   `entryID` int(10) unsigned NOT NULL DEFAULT '0',
   `userID` int(10) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`entryID`,`userID`),
   KEY `entryID` (`entryID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `favourites`
 --
 
 DROP TABLE IF EXISTS `favourites`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `favourites` (
   `userID` int(10) unsigned NOT NULL DEFAULT '0',
   `entryID` int(10) unsigned NOT NULL DEFAULT '0',
@@ -300,15 +267,12 @@ CREATE TABLE `favourites` (
   KEY `Favourites_entries` (`entryID`),
   KEY `lastVisited` (`lastVisited`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `feeds`
 --
 
 DROP TABLE IF EXISTS `feeds`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `feeds` (
   `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `feedURL` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
@@ -325,15 +289,12 @@ CREATE TABLE `feeds` (
   KEY `nextUpdate` (`nextUpdate`),
   KEY `status` (`status`)
 ) ENGINE=MyISAM AUTO_INCREMENT=97 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `friends`
 --
 
 DROP TABLE IF EXISTS `friends`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `friends` (
   `friendOf` int(10) unsigned NOT NULL DEFAULT '0',
   `userID` int(10) unsigned NOT NULL DEFAULT '0',
@@ -343,15 +304,12 @@ CREATE TABLE `friends` (
   KEY `friendOf` (`friendOf`),
   KEY `friendType` (`friendType`,`friendOf`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `lastvisits`
 --
 
 DROP TABLE IF EXISTS `lastvisits`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `lastvisits` (
   `userID` int(10) unsigned NOT NULL DEFAULT '0',
   `entryID` int(10) unsigned NOT NULL DEFAULT '0',
@@ -359,15 +317,12 @@ CREATE TABLE `lastvisits` (
   PRIMARY KEY (`userID`),
   KEY `lastVisits_entryID` (`entryID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `mail`
 --
 
 DROP TABLE IF EXISTS `mail`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `mail` (
   `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `Recipient` int(10) unsigned NOT NULL DEFAULT '0',
@@ -389,15 +344,12 @@ CREATE TABLE `mail` (
   KEY `Sender` (`Sender`,`Recipient`),
   KEY `isDeletedByRecipient` (`Recipient`,`isDeletedByRecipient`,`Date`)
 ) ENGINE=MyISAM AUTO_INCREMENT=660948 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `pollanswers`
 --
 
 DROP TABLE IF EXISTS `pollanswers`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `pollanswers` (
   `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `pollID` int(10) unsigned NOT NULL DEFAULT '0',
@@ -406,15 +358,12 @@ CREATE TABLE `pollanswers` (
   PRIMARY KEY (`ID`),
   KEY `PollAnswers_pollID` (`pollID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `polls`
 --
 
 DROP TABLE IF EXISTS `polls`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `polls` (
   `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `userID` int(10) unsigned NOT NULL DEFAULT '0',
@@ -425,29 +374,23 @@ CREATE TABLE `polls` (
   KEY `Polls_userID` (`userID`),
   KEY `Polls_entryID` (`entryID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `pollvotes`
 --
 
 DROP TABLE IF EXISTS `pollvotes`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `pollvotes` (
   `AnswerID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `UserID` int(10) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`AnswerID`,`UserID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `sessions`
 --
 
 DROP TABLE IF EXISTS `sessions`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `sessions` (
   `ID` bigint(15) NOT NULL DEFAULT '0',
   `userID` int(10) unsigned NOT NULL DEFAULT '0',
@@ -459,15 +402,12 @@ CREATE TABLE `sessions` (
   KEY `Sessions_userID` (`userID`),
   KEY `activationDate` (`activationDate`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `statistics`
 --
 
 DROP TABLE IF EXISTS `statistics`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `statistics` (
   `userID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `date` date NOT NULL DEFAULT '0000-00-00',
@@ -476,15 +416,12 @@ CREATE TABLE `statistics` (
   PRIMARY KEY (`userID`),
   KEY `Statistics_date` (`date`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `statisticspages`
 --
 
 DROP TABLE IF EXISTS `statisticspages`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `statisticspages` (
   `Action` char(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   `Date` date NOT NULL DEFAULT '0000-00-00',
@@ -493,15 +430,12 @@ CREATE TABLE `statisticspages` (
   PRIMARY KEY (`Action`),
   KEY `StatisticsPages_date` (`Date`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `userdata`
 --
 
 DROP TABLE IF EXISTS `userdata`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `userdata` (
   `userID` int(10) unsigned NOT NULL DEFAULT '0',
   `name` varchar(60) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
@@ -510,15 +444,12 @@ CREATE TABLE `userdata` (
   KEY `Index_1` (`userID`),
   KEY `Index_2` (`name`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `users`
 --
 
 DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
   `ID` int(6) unsigned NOT NULL AUTO_INCREMENT,
   `login` char(30) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
@@ -533,15 +464,12 @@ CREATE TABLE `users` (
   KEY `isTerminated` (`isTerminated`),
   KEY `activationDate` (`activationDate`)
 ) ENGINE=MyISAM AUTO_INCREMENT=9902 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `usertrack`
 --
 
 DROP TABLE IF EXISTS `usertrack`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `usertrack` (
   `sessionID` char(30) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   `userID` char(30) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
@@ -550,13 +478,15 @@ CREATE TABLE `usertrack` (
   `Action` char(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   PRIMARY KEY (`sessionID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+/* Restore saved variables in reverse order */
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
