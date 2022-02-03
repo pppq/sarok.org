846f3dcfa8bcc1566fbe9295c68c9ebd3f5a5529
pppq <pppq@users.noreply.github.com>
2022-02-03 23:26:00 +0100

    Remove unused code snippets from TextProcessor, enable built-in Tidy

---
+++ src/Sarok/TextProcessor.php
@@ -0,0 +1,91 @@
+<?php namespace Sarok;
+
+use tidy;
+
+class TextProcessor {
+
+    private const ALLOWED_TAGS = '<a><h1><h2><h3><center><i><b><span><div><ul><li><small><big><sup><sub><strike><u><input>' .
+                                 '<select><button><pre><hr><blockquote><code><img><object><param><embed><nowrap><select>' .
+                                 '<option><form><iframe><br>';
+    
+    private const WRONG_WINDOW = 'Tegnap megismerkedtem egy fiúval és azóta csak rá gondolok. Gyönyörű göndör haja van, kék ' .
+                                 'szeme és vastag farka, amely alig fér be a se... Bocs, azt hiszem rossz ablakba írtam!';
+
+    private const DEFAULT_TIDY_CONFIG = array(
+        'clean' => true,
+        'output-xhtml' => true,
+        'show-body-only' => true,
+        'wrap' => 120, 
+    );
+    
+    private Logger $log;
+
+    public function __construct(Logger $log) {
+        $this->log = $log;
+        $this->log->debug("TextProcessor initialized");
+    }
+
+    public function preFormat(string $text) : string {
+        $this->log->debug("Preformatting '$text'");
+        
+        if (isset($text) && strlen($text) > 0) {
+            $text = strip_tags($text, self::ALLOWED_TAGS);
+            $text = str_replace('!!!!!!!!!!!!!!!', 'Idióta vagyok! Valaki lőjön le engem!', $text);
+            $text = str_replace('???????????????', 'Idióta vagyok? Valaki lőjön le engem!', $text);
+            $text = str_replace(' lol ', self::WRONG_WINDOW, $text);
+            
+            $text = preg_replace('/!!![! \n\r]+/', '!!!', $text);
+            $text = preg_replace('/\?\?\?[\? \n\r]+/', '???', $text);
+            $text = preg_replace('/\?!\?![\?\! \n\r]+/', '?!?!', $text);
+    
+            $text = str_replace('(c)', '&copy;', $text);
+            $text = str_replace('(r)', '&reg;', $text);
+            $text = str_replace('(tm)', '&trade;', $text);
+        }
+        
+        $this->log->debug("Done preformatting '$text'");
+        return $text;
+    }
+
+    public function postFormat(string $text, string $searchKeyword = '') : string {
+        $this->log->debug("Post-formatting '$text'");
+        
+        if (isset($text) && strlen($text) > 0) {
+            $text = str_replace(' -- ', ' &ndash; ', $text);
+            $text = str_replace(',-- ', ',&ndash; ', $text);
+            $text = str_replace(' --,', ' &ndash;,', $text);
+            
+            // Auto-link to user's profile page
+            $text = preg_replace('/uid_([A-Za-z0-9]+)/', '<a href="/users/\\1/" class="personid">\\1</a>', $text);
+        
+            /* 
+             * Regexp detecting non-HTML tag sections: from CakePHP
+             * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+             * Licensed under The MIT License
+             * 
+             * https://github.com/cakephp/cakephp/blob/3cce4816095cfb0bb858b4421df34511c9277458/src/Utility/Text.php#L531-L540
+             */
+            if (isset($searchKeyword) && strlen($searchKeyword) > 0) {
+                $searchKeyword = '/(?![^<]+>)(' . preg_quote($searchKeyword, '/') . ')(?![^<]+>)/iu';
+                $text = preg_replace($searchKeyword, '<span class="search">\\1</span>', $text);
+            }
+        }
+        
+        $this->log->debug("Done post-formatting '$text'");
+        return $text;
+    }
+
+    public function tidy(string $text, array $tidy_config = self::DEFAULT_TIDY_CONFIG) : string {
+        $this->log->debug("HTML Tidy-ing '$text'");
+        
+        if (isset($text) && strlen($text) > 0) {
+            $tidy = new tidy();
+            $tidy->parseString($text, $tidy_config, 'utf8');
+            $tidy->cleanRepair();
+            $text = tidy_get_output($tidy); 
+        }
+        
+        $this->log->debug("Done HTML Tidy-ing '$text'");
+        return $text;
+    }
+}
