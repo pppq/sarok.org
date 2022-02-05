e232a274067135ac2aacf95e657a1c37c2cd26e2
pppq <pppq@users.noreply.github.com>
2022-02-05 18:14:30 +0100

    Update default parameters in Exception constructors

--- src/Sarok/Exceptions/DBException.php
+++ src/Sarok/Exceptions/DBException.php
@@ -7,7 +7,7 @@ use Exception;
  */
 class DBException extends Exception {
     
-    public function __construct($message = null, $code = null, $previous = null) {
+    public function __construct($message = '', $code = 0, $previous = null) {
         parent::__construct($message, $code, $previous);
     }
 }
--- src/Sarok/Exceptions/LogException.php
+++ src/Sarok/Exceptions/LogException.php
@@ -7,7 +7,7 @@ use Exception;
  */
 class LogException extends Exception {
 
-    public function __construct($message = null, $code = null, $previous = null) {
+    public function __construct($message = '', $code = 0, $previous = null) {
         parent::__construct($message, $code, $previous);
     }
 }
