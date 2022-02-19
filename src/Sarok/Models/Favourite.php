0968e5d01fdba597a4efc956667b43e1c2a53d24
pppq <pppq@users.noreply.github.com>
2022-02-19 23:19:10 +0100

    Use associative array for Favourite data, fix field references in repository

--- src/Sarok/Models/Favourite.php
+++ src/Sarok/Models/Favourite.php
@@ -3,8 +3,16 @@
 use Sarok\Util;
 use DateTime;
 
-class Favourite {
-
+/*
+ * Table structure for `favourites`:
+ * 
+ * `userID`      int(10) unsigned NOT NULL DEFAULT '0',
+ * `entryID`     int(10) unsigned NOT NULL DEFAULT '0',
+ * `lastVisited` datetime         NOT NULL DEFAULT '0000-00-00 00:00:00',
+ * `newComments` int(10) unsigned NOT NULL DEFAULT '0',
+ */
+class Favourite
+{
     const FIELD_USER_ID = 'userID';
     const FIELD_ENTRY_ID = 'entryID';
     const FIELD_LAST_VISITED = 'lastVisited';
@@ -18,57 +26,68 @@ class Favourite {
     private int $entryID = 0;
     private int $newComments = 0;
 
-    public function __construct() {
+    public function __construct()
+    {
         if (!isset($this->_lastVisited)) {
             $this->_lastVisited = Util::utcDateTimeFromString();
         }
     }
     
-    public function __set(string $name, $value) {
+    public function __set(string $name, $value)
+    {
         // Support conversion from string for fetch_object()
         if ($name === self::FIELD_LAST_VISITED && is_string($value)) {
             $this->setDatum(Util::utcDateTimeFromString($value));
         }
     }
 
-    public function getUserID() : int {
+    public function getUserID() : int
+    {
         return $this->userID;
     }
 
-    public function setUserID(int $userID) {
+    public function setUserID(int $userID)
+    {
         $this->userID = $userID;
     }
     
-    public function getEntryID() : int {
+    public function getEntryID() : int
+    {
         return $this->entryID;
     }
 
-    public function setEntryID(int $entryID) {
+    public function setEntryID(int $entryID)
+    {
         $this->entryID = $entryID;
     }
     
-    public function getLastVisited() : DateTime {
+    public function getLastVisited() : DateTime
+    {
         return $this->_lastVisited;
     }
     
-    public function setLastVisited(DateTime $lastVisited) {
+    public function setLastVisited(DateTime $lastVisited)
+    {
         $this->_lastVisited = $lastVisited;
     }
     
-    public function getNewComments() : int {
+    public function getNewComments() : int
+    {
         return $this->newComments;
     }
     
-    public function setNewComments(int $newComments) {
+    public function setNewComments(int $newComments)
+    {
         $this->newComments = $newComments;
     }
 
-    public function toArray() : array {
+    public function toArray() : array
+    {
         return array(
-            $this->userID,
-            $this->entryID,
-            Util::dateTimeToString($this->_lastVisited),
-            $this->newComments,
+            self::FIELD_USER_ID      => $this->userID,
+            self::FIELD_ENTRY_ID     => $this->entryID,
+            self::FIELD_LAST_VISITED => Util::dateTimeToString($this->_lastVisited),
+            self::FIELD_NEW_COMMENTS => $this->newComments,
         );
     }
 }
--- src/Sarok/Repository/FavouriteRepository.php
+++ src/Sarok/Repository/FavouriteRepository.php
@@ -6,8 +6,8 @@ use Sarok\Service\DB;
 use Sarok\Util;
 use DateTime;
 
-class FavouriteRepository extends AbstractRepository {
-
+class FavouriteRepository extends AbstractRepository
+{
     const TABLE_NAME = 'favourites';
     
     private const COLUMN_NAMES = array(
@@ -17,22 +17,26 @@ class FavouriteRepository extends AbstractRepository {
         Favourite::FIELD_NEW_COMMENTS,
     );
     
-    public function __construct(DB $db) {
+    public function __construct(DB $db)
+    {
         parent::__construct($db);
     }
     
-    protected function getTableName() : string {
+    protected function getTableName() : string
+    {
         return self::TABLE_NAME;
     }
     
-    protected function getColumnNames() : array {
+    protected function getColumnNames() : array
+    {
         return self::COLUMN_NAMES;
     }
     
-    public function getFavouritedEntries(string $userID, bool $commentedAfterLastVisit) : array {
+    public function getFavouritedEntries(string $userID, bool $commentedAfterLastVisit) : array
+    {
         $favourites = $this->getTableName();
-        $entryIDColumn = EntryAccess::FIELD_ENTRY_ID;
-        $userIDColumn = Favourite::FIELD_ENTRY_ID;
+        $entryIDColumn = Favourite::FIELD_ENTRY_ID;
+        $userIDColumn = Favourite::FIELD_USER_ID;
         $lastVisited = Favourite::FIELD_LAST_VISITED;
         
         if ($commentedAfterLastVisit) {
@@ -54,10 +58,11 @@ class FavouriteRepository extends AbstractRepository {
         return $entryIDList;
     }
 
-    public function updateLastVisited(int $userID, int $entryID, DateTime $lastVisited) : int {
+    public function updateLastVisited(int $userID, int $entryID, DateTime $lastVisited) : int
+    {
         $favourites = $this->getTableName();
         $lastVisitedColumn = Favourite::FIELD_LAST_VISITED;
-        $userIDColumn = Favourite::FIELD_ENTRY_ID;
+        $userIDColumn = Favourite::FIELD_USER_ID;
         $entryIDColumn = Favourite::FIELD_ENTRY_ID;
         
         $q = "UPDATE `$favourites` SET `$lastVisitedColumn` = ? WHERE `$userIDColumn` = ? AND `$entryIDColumn` = ? LIMIT 1";
@@ -65,24 +70,28 @@ class FavouriteRepository extends AbstractRepository {
         return $this->db->execute($q, 'sii', $lastVisitedString, $userID, $entryID);
     }
     
-    public function delete(int $userID, int $entryID) : int {
+    public function delete(int $userID, int $entryID) : int
+    {
         $favourites = $this->getTableName();
-        $userIDColumn = Favourite::FIELD_ENTRY_ID;
+        $userIDColumn = Favourite::FIELD_USER_ID;
         $entryIDColumn = Favourite::FIELD_ENTRY_ID;
         
         $q = "DELETE FROM `$favourites` WHERE `$userIDColumn` = ? AND `$entryIDColumn` = ? LIMIT 1";
         return $this->db->execute($q, 'ii', $userID, $entryID);
     }
     
-    public function insert(Favourite $data) : int {
+    public function insert(Favourite $data) : int
+    {
         $favourites = $this->getTableName();
-        $insertColumns = $this->getColumnNames();
+        $favouriteArray = $data->toArray();
+        $insertColumns = array_keys($favouriteArray);
         $columnList = $this->toColumnList($insertColumns);
         $placeholderList = $this->toPlaceholderList($insertColumns);
         $lastVisited = Favourite::FIELD_LAST_VISITED;
         
         $q = "INSERT INTO `$favourites`(`$columnList`) VALUES ($placeholderList) ON DUPLICATE KEY UPDATE `$lastVisited` = ?";
-        $values = $data->toArray();
+        $values = array_values($favouriteArray);
+        // Add extra datetime value and 's' parameter type for ON DUPLICATE KEY UPDATE
         $values[] = Util::dateTimeToString($data->getLastVisited());
         return $this->db->execute($q, 'iisis', ...$values);
     }
