929ca61d0b9b069f588ec6dd0e1f46d756a05835
pppq <pppq@users.noreply.github.com>
2022-02-19 22:21:33 +0100

    Use associative array for Message data

--- src/Sarok/Models/Message.php
+++ src/Sarok/Models/Message.php
@@ -3,8 +3,22 @@
 use Sarok\Util;
 use DateTime;
 
-class Message {
-
+/*
+ * Table structure for `mail`:
+ *
+ * `ID`        int(10) unsigned NOT NULL AUTO_INCREMENT,
+ * `Recipient` int(10) unsigned NOT NULL DEFAULT '0',
+ * `Sender`    int(10) unsigned NOT NULL DEFAULT '0',
+ * `Date`      datetime         NOT NULL DEFAULT '0000-00-00 00:00:00',
+ * `Title`     varchar(255)     NOT NULL DEFAULT '',
+ * `Body`      longtext         NOT NULL DEFAULT '',
+ * `isRead`    enum('Y','N')    NOT NULL DEFAULT 'N',
+ * `isDeletedByRecipient` enum('Y','N') NOT NULL DEFAULT 'N',
+ * `isDeletedBySender`    enum('Y','N') NOT NULL DEFAULT 'N',
+ * `replyOn`   int(10) unsigned NOT NULL DEFAULT '0',
+ */
+class Message
+{
     const FIELD_ID = 'ID';
     const FIELD_RECIPIENT = 'Recipient';
     const FIELD_SENDER = 'Sender';
@@ -14,7 +28,7 @@ class Message {
     const FIELD_IS_READ = 'isRead';
     const FIELD_IS_DELETED_BY_RECIPIENT = 'isDeletedByRecipient';
     const FIELD_IS_DELETED_BY_SENDER = 'isDeletedBySender';
-    const REPLY_ON = 'replyOn';
+    const FIELD_REPLY_ON = 'replyOn';
     
     // Assignment requires conversion via magic method (__set)
     private DateTime $_Date;
@@ -30,14 +44,16 @@ class Message {
     private string $Body = '';
     private int $replyOn = 0;
     
-    public function __construct() {
+    public function __construct()
+    {
         if (!isset($this->_lastUpdate)) {
             // Default is 'zero date', but 'now' is more appropriate
             $this->_date = Util::utcDateTimeFromString();
         }
     }
     
-    public function __set(string $name, $value) {
+    public function __set(string $name, $value)
+    {
         // Support conversion from string for fetch_object()
         if ($name === self::FIELD_DATE && is_string($value)) {
             $this->setDate(Util::utcDateTimeFromString($value));
@@ -56,98 +72,119 @@ class Message {
         }
     }
 
-    public function getID() : int {
+    public function getID() : int
+    {
         return $this->ID;
     }
 
-    public function setID(int $ID) {
+    public function setID(int $ID)
+    {
         $this->ID = $ID;
     }
 
-    public function getRecipient() : int {
+    public function getRecipient() : int
+    {
         return $this->recipient;
     }
 
-    public function setRecipient(int $recipient) {
+    public function setRecipient(int $recipient)
+    {
         $this->recipient = $recipient;
     }
 
-    public function getSender() : int {
+    public function getSender() : int
+    {
         return $this->sender;
     }
 
-    public function setSender(int $sender) {
+    public function setSender(int $sender)
+    {
         $this->sender = $sender;
     }
 
-    public function getDate() : DateTime {
+    public function getDate() : DateTime
+    {
         return $this->_date;
     }
     
-    public function setDate(DateTime $date) {
+    public function setDate(DateTime $date)
+    {
         $this->_date = $date;
     }
     
-    public function getTitle() : string {
+    public function getTitle() : string
+    {
         return $this->title;
     }
 
-    public function setTitle(string $title) {
+    public function setTitle(string $title)
+    {
         $this->title = $title;
     }
 
-    public function getBody() : string {
+    public function getBody() : string
+    {
         return $this->body;
     }
 
-    public function setBody(string $body) {
+    public function setBody(string $body)
+    {
         $this->body = $body;
     }
 
-    public function isRead() : bool {
+    public function isRead() : bool
+    {
         return $this->read;
     }
 
-    public function setRead(bool $read) {
+    public function setRead(bool $read)
+    {
         $this->read = $read;
     }
 
-    public function getDeletedByRecipient() : bool {
+    public function getDeletedByRecipient() : bool
+    {
         return $this->deletedByRecipient;
     }
 
-    public function setDeletedByRecipient(bool $deletedByRecipient) {
+    public function setDeletedByRecipient(bool $deletedByRecipient)
+    {
         $this->deletedByRecipient = $deletedByRecipient;
     }
 
-    public function getDeletedBySender() : bool {
+    public function getDeletedBySender() : bool
+    {
         return $this->deletedBySender;
     }
 
-    public function setDeletedBySender(bool $deletedBySender) {
+    public function setDeletedBySender(bool $deletedBySender)
+    {
         $this->deletedBySender = $deletedBySender;
     }
 
-    public function getReplyOn() : int {
+    public function getReplyOn() : int
+    {
         return $this->replyOn;
     }
 
-    public function setReplyOn(int $replyOn) {
+    public function setReplyOn(int $replyOn)
+    {
         $this->replyOn = $replyOn;
     }
     
-    public function toArray() : array {
+    public function toArray() : array
+    {
         return array(
-            $this->ID, // 0
-            $this->recipient, // 1
-            $this->sender, // 2
-            Util::dateTimeToString($this->_date), // 3
-            $this->title, // 4
-            $this->body, // 5
-            Util::boolToYesNo($this->read),
-            Util::boolToYesNo($this->deletedByRecipient),
-            Util::boolToYesNo($this->deletedBySender),
-            $this->replyOn,
+            self::FIELD_ID        => $this->ID,
+            self::FIELD_RECIPIENT => $this->recipient,
+            self::FIELD_SENDER    => $this->sender,
+            self::FIELD_DATE      => Util::dateTimeToString($this->_date),
+            self::FIELD_TITLE     => $this->title,
+            self::FIELD_BODY      => $this->body,
+            self::FIELD_IS_READ   => Util::boolToYesNo($this->read),
+            self::FIELD_IS_DELETED_BY_RECIPIENT => Util::boolToYesNo($this->deletedByRecipient),
+            self::FIELD_IS_DELETED_BY_SENDER => Util::boolToYesNo($this->deletedBySender),
+            self::FIELD_REPLY_ON  => $this->replyOn,
         );
     }
 }
--- src/Sarok/Repository/MessageRepository.php
+++ src/Sarok/Repository/MessageRepository.php
@@ -6,8 +6,8 @@ use Sarok\Service\DB;
 use DateTime;
 use InvalidArgumentException;
 
-class MessageRepository extends AbstractRepository {
-
+class MessageRepository extends AbstractRepository
+{
     const TABLE_NAME = 'mail';
     
     private const COLUMN_NAMES = array(
@@ -23,19 +23,23 @@ class MessageRepository extends AbstractRepository {
         Message::REPLY_ON,
     );
     
-    public function __construct(DB $db) {
+    public function __construct(DB $db)
+    {
         parent::__construct($db);
     }
     
-    protected function getTableName() : string {
+    protected function getTableName() : string
+    {
         return self::TABLE_NAME;
     }
     
-    protected function getColumnNames() : array {
+    protected function getColumnNames() : array
+    {
         return self::COLUMN_NAMES;
     }
     
-    public function getUnreadMessage(int $recipientID, bool $latestOrEarliest) : Message {
+    public function getUnreadMessage(int $recipientID, bool $latestOrEarliest) : Message
+    {
         $selectColumns = array(
             Message::FIELD_ID,
             Message::FIELD_SENDER,
@@ -51,7 +55,7 @@ class MessageRepository extends AbstractRepository {
         $date = Message::FIELD_DATE;
         $order = $latestOrEarliest ? 'ASC' : 'DESC';
         
-        $q = "SELECT `$selectColumnList` FROM `$mail` " . 
+        $q = "SELECT `$selectColumnList` FROM `$mail` " .
             "WHERE `$recipient` = ? AND `$isRead` = 'N' AND `$isDeletedByRecipient` = 'N'" .
             "ORDER BY `$date` $order LIMIT 1";
         
@@ -59,7 +63,8 @@ class MessageRepository extends AbstractRepository {
         return $messages[0];
     }
     
-    public function getUnreadMessageCount(int $recipientID) : int {
+    public function getUnreadMessageCount(int $recipientID) : int
+    {
         $id = Message::FIELD_ID;
         $mail = $this->getTableName();
         $recipient = Message::FIELD_RECIPIENT;
@@ -77,7 +82,8 @@ class MessageRepository extends AbstractRepository {
         return 0;
     }
     
-    public function getPartners(int $recipientID) : array {
+    public function getPartners(int $recipientID) : array
+    {
         $sender = Message::FIELD_SENDER;
         $id = Message::FIELD_ID;
         $mail = $this->getTableName();
@@ -100,7 +106,8 @@ class MessageRepository extends AbstractRepository {
         return $partnerList;
     }
 
-    public function getParticipants(int $mailID) : array {
+    public function getParticipants(int $mailID) : array
+    {
         $sender = Message::FIELD_SENDER;
         $recipient = Message::FIELD_RECIPIENT;
         $mail = $this->getTableName();
@@ -118,7 +125,8 @@ class MessageRepository extends AbstractRepository {
         return array();
     }
     
-    public function getMessage(string $encryptionKey, int $messageID, int $participantID) : ?Message {
+    public function getMessage(string $encryptionKey, int $messageID, int $participantID) : ?Message
+    {
         if (!isset($encryptionKey) || strlen($encryptionKey) <= 0) {
             throw new InvalidArgumentException("Encryption key is required for retrieving messages.");
         }
@@ -143,10 +151,10 @@ class MessageRepository extends AbstractRepository {
             "(`$recipient` = ? AND `$isDeletedByRecipient` = 'N' OR " .
             "`$sender` = ? AND `$isDeletedBySender` = 'N')";
         
-        $message = $this->db->queryObjects($q, Message::class, 'siii', 
-            $encryptionKey, 
-            $messageID, 
-            $participantID, 
+        $message = $this->db->queryObjects($q, Message::class, 'siii',
+            $encryptionKey,
+            $messageID,
+            $participantID,
             $participantID);
         
         if (count($message) > 0) {
@@ -156,7 +164,8 @@ class MessageRepository extends AbstractRepository {
         return null;
     }
     
-    public function findMessages(string $encryptionKey, array $filters, int $offset = 0, int $limit = 50) : array {
+    public function findMessages(string $encryptionKey, array $filters, int $offset = 0, int $limit = 50) : array
+    {
         if (!isset($encryptionKey) || strlen($encryptionKey) <= 0) {
             throw new InvalidArgumentException("Encryption key is required for retrieving messages.");
         }
@@ -219,24 +228,34 @@ class MessageRepository extends AbstractRepository {
         return $this->db->queryObjectsWithParams($q, Message::class, $params);
     }
     
-    public function setBooleanFlag(int $mailID, string $fieldName) : int {
+    private function setBooleanFlag(int $mailID, string $fieldName) : bool
+    {
         $mail = $this->getTableName();
         $id = Message::FIELD_ID;
-
-        /*
-         * Field name should be one of the following:
-         * 
-         * - Message::FIELD_IS_READ
-         * - Message::FIELD_IS_DELETED_BY_RECIPIENT
-         * - Message::FIELD_IS_DELETED_BY_SENDER
-         */
         $q = "UPDATE `$mail` SET `$fieldName` = 'Y' WHERE `$id` = ? LIMIT 1";
-        return $this->db->execute($q, 'i', $mailID);
+        return $this->db->execute($q, 'i', $mailID) > 0;
+    }
+
+    public function markRead(int $mailID) : bool
+    {
+        return $this->setBooleanFlag($mailID, Message::FIELD_IS_READ);
+    }
+
+    public function markDeletedByRecipient(int $mailID) : bool
+    {
+        return $this->setBooleanFlag($mailID, Message::FIELD_IS_DELETED_BY_RECIPIENT);
+    }
+
+    public function markDeletedBySender(int $mailID) : bool
+    {
+        return $this->setBooleanFlag($mailID, Message::FIELD_IS_DELETED_BY_SENDER);
     }
     
-    public function insert(string $encryptionKey, Message $data) : int {
+    public function insert(string $encryptionKey, Message $data) : int
+    {
         $mail = $this->getTableName();
-        $insertColumns = $this->getColumnNames();
+        $messageArray = $data->toArray();
+        $insertColumns = array_keys($messageArray);
         $columnList = $this->toColumnList($insertColumns);
 
         // Body text should be encrypted; modify placeholder
@@ -245,9 +264,9 @@ class MessageRepository extends AbstractRepository {
         $placeholderList = implode(', ', $placeholders);
         
         $q = "INSERT INTO `$mail`(`$columnList`) VALUES ($placeholderList)";
-        $values = $data->toArray();
+        $values = array_values($messageArray);
         
-        // Insert key to values after body text (index 5)
+        // Insert key to values after field 'body' (index 5)
         array_splice($values, 6, 0, $encryptionKey);
         
         // Type string is also extended with an extra 's' for the key
