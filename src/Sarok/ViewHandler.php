cafdd9cec45e7d893351cc84bf3501a642cd6dbc
pppq <pppq@users.noreply.github.com>
2022-03-14 14:34:06 +0100

    Add type hints and format ViewHandler

--- src/Sarok/ViewHandler.php
+++ src/Sarok/ViewHandler.php
@@ -1,25 +1,38 @@
 <?php namespace Sarok;
 
-class ViewHandler {
-    
-    // tile name => array of actions
-    private array $tiles = array();
-    // action name => action data (as an associative array) 
+class ViewHandler
+{
+    /** 
+     * tile name => array of actions
+     * @var array<string, array<string>> 
+     */
+    private array $actions = array();
+
+    /**
+     * action name => action data (as an associative array)
+     * @var array<string, mixed>
+     */ 
     private array $data = array();
-    // tile name => tile content + any extra variables used in the main template (index.php)
+    
+    /**
+     * tile name => tile content + any extra variables used in the main template (index.php)
+     * @var array<string, mixed>
+     */
     private array $viewData = array();
     
     private string $templateName;
     private TemplateRenderer $templateRenderer;
     private Logger $logger;
 
-    public function __construct(Logger $logger, TemplateRenderer $templateRenderer) {
+    public function __construct(Logger $logger, TemplateRenderer $templateRenderer)
+    {
         $this->logger = $logger;
         $this->templateRenderer = $templateRenderer;
         $this->logger->debug("ViewHandler initialized");
     }
 
-    public function setTemplate(string $templateName) {
+    public function setTemplate(string $templateName) : void
+    {
         if (file_exists("../templates/$templateName/index.php")) {
             $this->logger->debug("Using template '$templateName'");
             $this->templateName = $templateName;
@@ -29,26 +42,46 @@ class ViewHandler {
         }
     }
 
-    public function addAction(string $tile, string $action) {
-        $this->tiles[$tile][] = $action;
+    public function addAction(string $tile, string $action) : void
+    {
+        if (!isset($this->actions[$tile])) {
+            $this->actions[$tile] = array($action);
+        } else {
+            $this->actions[$tile][] = $action;
+        }
     }
     
-    public function setData(array $actions) {
+    /**
+     * @param array<string, mixed> $actions
+     */
+    public function setData(array $actions) : void
+    {
         foreach ($actions as $action => $variables) {
             $this->data[$action] = $variables;
         }
     }
 
-    public function setViewData(string $key, $value) {
-        $this->viewData[$key] = $value;        
+    /**
+     * @param string $key
+     * @param mixed $value
+     */
+    public function setViewData(string $key, $value) : void
+    {
+        $this->viewData[$key] = $value;
     }
     
-    public function mergeViewData(array $viewData) {
-        array_merge($this->viewData, $viewData);
+    /**
+     * @param array<string, mixed> $viewData
+     */
+    public function mergeViewData(array $viewData) : void
+    {
+        $this->viewData = array_merge($this->viewData, $viewData);
     }
     
-    private function renderAction($action) : string {
+    private function renderAction(string $action) : string
+    {
         $templatePath = "../templates/{$this->templateName}/$action.php";
+
         if (file_exists($templatePath)) {
             $this->logger->debug("Using action template '$templatePath'");
         } else {
@@ -59,11 +92,15 @@ class ViewHandler {
         return $this->templateRenderer->render($templatePath, $this->data[$action]);
     }
 
-    public function render() : string {
+    public function render() : string
+    {
         // Concatenate rendered output for each action in tile
-        foreach ($this->tiles as $tile => $actions) {
-            $this->viewData[$tile] = "";
-            foreach ($actions as $action) {
+        foreach ($this->actions as $tile => $tileActions) {
+            if (!isset($this->viewData[$tile])) {
+                $this->viewData[$tile] = "";
+            }
+            
+            foreach ($tileActions as $action) {
                 $this->viewData[$tile] .= $this->renderAction($action);
             }
         }
