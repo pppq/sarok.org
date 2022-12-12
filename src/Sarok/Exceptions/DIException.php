b408d1622de2760a5407c1f226370b0449c11826
pppq <pppq@users.noreply.github.com>
2022-12-12 23:51:56 +0100

    Enable strict types for exceptions

--- src/Sarok/Exceptions/DIException.php
+++ src/Sarok/Exceptions/DIException.php
@@ -1,13 +1,17 @@
-<?php namespace Sarok\Exceptions;
+<?php declare(strict_types=1);
+
+namespace Sarok\Exceptions;
 
 use Exception;
+use Throwable;
 
 /**
  * Thrown when an issue is encountered during the dependency injection process.
  */
-class DIException extends Exception {
-
-    public function __construct($message = '', $code = 0, $previous = null) {
+class DIException extends Exception 
+{
+    public function __construct(string $message = '', int $code = 0, ?Throwable $previous = null) 
+    {
         parent::__construct($message, $code, $previous);
     }
 }
--- src/Sarok/Exceptions/LogException.php
+++ src/Sarok/Exceptions/LogException.php
@@ -1,13 +1,17 @@
-<?php namespace Sarok\Exceptions;
+<?php declare(strict_types=1);
+
+namespace Sarok\Exceptions;
 
 use Exception;
+use Throwable;
 
 /**
  * Thrown when an issue is encountered while adding an entry to the application log.
  */
-class LogException extends Exception {
-
-    public function __construct($message = '', $code = 0, $previous = null) {
+class LogException extends Exception 
+{
+    public function __construct(string $message = '', int $code = 0, ?Throwable $previous = null) 
+    {
         parent::__construct($message, $code, $previous);
     }
 }
